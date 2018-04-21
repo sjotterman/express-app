ca9590e0443e29c03a29b1cce5f5abc40958a5e2
Samuel Otterman <sjotterman@gmail.com>
2018-04-21 15:14:05 -0400

    can mock DELETE requests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './index.css';
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 getUsers().then(result => {
   let usersBody = "";
 
   result.forEach(user => {
     usersBody += `<tr>
-    <td><a href="#" data-id=${user.id}" class="deleteUser">Delete</a></td>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
     <td>${user.id}</td>
     <td>${user.firstName}</td>
     <td>${user.lastName}</td>
@@ -14,4 +14,18 @@ getUsers().then(result => {
     <tr>`;
   });
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks =  global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassName only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
