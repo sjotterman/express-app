9894fb0cbb2a368087163bd932e36a2a20ea5dc5
Samuel Otterman <sjotterman@gmail.com>
2018-04-21 19:38:46 -0400

    bundle vendor libraries separately in vendor.js

---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+/*
+ This file contains references to vendor libraries used in this project. This is used by
+ webpack in the production build only. This allows caching of vendor code so it doesn't have
+ to be redownloaded every time application code changes. Any files that aren't referenced
+ here will be bundled into main.js for the production build.
+*/
+
+/* eslint-disable no-unused-vars */
+import fetch from 'whatwg-fetch';
