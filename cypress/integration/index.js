ecb26ba917f7ff20dedf5106f74c5738b2ff5afd
Samuel Otterman <sjotterman@gmail.com>
2018-05-07 22:04:27 -0400

    change title

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -16,14 +16,13 @@ describe('Index page', function () {
     // https://on.cypress.io/visit
     cy.visit('http://localhost:3000')
 
-    cy.title().should('include', 'Starter Project')
+    cy.title().should('include', 'Recipes')
       cy.get('#starter-h1').should('contain', 'Bootstrap starter template')
       cy.get('#users-api-title').should('contain', 'Example users API call')
   })
 
   it('should register button click', function () {
     cy.visit('http://localhost:3000')
-    cy.title().should('include', 'Starter Project')
       cy.get('#starter-h1').should('contain', 'Bootstrap starter template')
       cy.get('#frontpage-test-btn').should('contain', 'Click me!')
       cy.get('#frontpage-test-btn').click()
